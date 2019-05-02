a443fb169cc11983fc7ad3decbcbeaa27569c3cb
Matthew Bowers <matbowrs@gmail.com>
2019-05-02 02:52:51 -0400

    Home route (aka default route) added

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
 
-
+const homeRoute = require('./api/routes/home');
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
 // Anything starting with /products in URL will be forwarded to 
 // products.js file, so we do not need /products in the products.js
 // It's like saying /products/products
+
+app.use('', homeRoute);
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
