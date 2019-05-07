2fa739dbec35592fe3060b58a8dbc17cd73ab47e
Matthew Bowers <matbowrs@gmail.com>
2019-05-07 15:29:54 -0400

    Handling CORS errors and updated orders and products POST requests (returns a json object with order/product created).

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const homeRoute = require('./api/routes/home');
 const productRoutes = require('./api/routes/products');
@@ -9,10 +10,30 @@ const orderRoutes = require('./api/routes/orders');
 // Logging package
 app.use(morgan('dev'));
 
+// body-parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+// Handling CORS errors
+// ... what?
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers', 'Origin, X-Requested-With', 
+        'Content-Type, Accept, Authorization'
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        
+        return res.status(200).json({});
+    }
+
+})
+
 // Anything starting with /products in URL will be forwarded to 
 // products.js file, so we do not need /products in the products.js
 // It's like saying /products/products
-
 app.use('', homeRoute);
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
